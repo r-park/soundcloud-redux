4bc83e55eb4937e34cef6f1983408f176a0b9b73
Richard Park <objectiv@gmail.com>
2017-07-28 16:14:42 -0400

    fix(components): update import paths

--- src/views/components/track-card/index.js
+++ src/views/components/track-card/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import classNames from 'classnames';
 import { Track } from 'src/core/tracks';
 
